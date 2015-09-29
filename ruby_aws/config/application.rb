06977074c7e895ef0d52be394b25f91bb0c8e67e
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-29 04:48:05 -0400

    added the config setting in application.rb

--- ruby_aws/config/application.rb
+++ ruby_aws/config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module RubyAws
   class Application < Rails::Application
+    config.action_mailer.delivery_method = :aws_sdk
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
