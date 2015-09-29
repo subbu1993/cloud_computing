bc28181af98e9cc844ee95ef19730fd2feb71304
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-29 07:28:46 -0400

    new env variables set

--- ruby_aws/app/controllers/aws_controller.rb
+++ ruby_aws/app/controllers/aws_controller.rb
@@ -1,6 +1,6 @@
 class AwsController < ApplicationController
   def run_instance
-    @credentials =  Aws::Credentials.new('AKIAISBJCPQTW3M5RLFQ', 'zNmsLEKX+BTvPXSqTJa/h1a/dYYYjn6J3nIg41a1')
+    @credentials =  Aws::Credentials.new(ENV['AWS_ACCES_KEY'], ENV['AWS_SECURITY_ACCESS_ID'])
     @return_from_config = Aws.config.update({
       region: 'us-west-2',
       credentials: @credentials,
@@ -12,13 +12,14 @@ class AwsController < ApplicationController
     @ec2 = Aws::EC2::Client.new(region:'us-west-2', credentials: @credentials)
 
     @run_an_instance = @ec2.run_instances({
-      dry_run: true,
       image_id: "ami-9ff7e8af",
       min_count: 1,
       max_count: 1,
       security_groups: ["launch-wizard-1"],
       instance_type: "t2.micro",
     })
+
+    @run_instances.descibe_instance_status ==
 
   end
 end
