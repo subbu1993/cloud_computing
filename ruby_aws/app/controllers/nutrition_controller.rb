d14d33601269715990d9993d54973a5f23c7a3ac
raghavrav4 <rr2731@nyu.edu>
2015-10-21 20:05:03 -0400

    all done

--- ruby_aws/app/controllers/nutrition_controller.rb
+++ ruby_aws/app/controllers/nutrition_controller.rb
@@ -1,9 +1,13 @@
-require 'nutrition_helper'
 class NutritionController < ApplicationController
   include NutritionHelper
   def show
     @passed_in = params[:q]
-    @numbers = 100.rand_sum 4
+    numbers = rand_sum 4
+    @numbers_0 = numbers[0]
+    @numbers_1 = numbers[1]
+    @numbers_2 = numbers[2]
+    @numbers_3 = numbers[3]
+
   end
 
 end
--- ruby_aws/app/helpers/nutrition_helper.rb
+++ ruby_aws/app/helpers/nutrition_helper.rb
@@ -1,11 +1,12 @@
 module NutritionHelper
-  def rand_sum(n = 2)
+  def rand_sum(n = 4)
    arr = (n - 1).times.reduce([]) do |a, _|
-     curr_max = self - a.reduce(0, :+)
+     curr_max = 100 - a.reduce(0, :+)
      a << rand(0..curr_max)
    end
 
-   arr << self - arr.reduce(0, :+)
+   arr << 100 - arr.reduce(0, :+)
+   return arr
  end
 
 end
